e53177b60e60e33be79830e3704cba881f0d1813
Tyler Kummer <tylerkummer@protonmail.com>
2020-07-21 15:37:18 -0500

    Comments to searching

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,19 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    # Make sure the start of the array is not bigger than the end of the array for the binary search tree
     if start <= end:
+        # Create a middle index based on the start and end where we floor the division
         mid = (start + end) // 2
 
+        # If the target element is in the middle of the array then return it
         if arr[mid] == target:
             return mid
 
+        # Checks if the target is smalled than mid, and places it in the left subtree.
         elif arr[mid] > target:
             return binary_search(arr, target, start, mid - 1)
+        # Target must be bigger than middle index so it goes in the right subtree
         else:
             return binary_search(arr, target, mid + 1, end)
 
